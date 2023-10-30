3ae5d775d6a831a2d1734742a90addbee6edd438
BackFoxx <backfoxxx@gmail.com>
2023-10-30 13:05:46 +0900

    init

---
+++ replace/src/main/kotlin/com/app/replace/ReplaceApplication.kt
@@ -0,0 +1,11 @@
+package com.app.replace
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReplaceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReplaceApplication>(*args)
+}
