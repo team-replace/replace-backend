db8c5a3df11b099d7b0eeb20cb8c541deffbbade
BackFoxx <backfoxxx@gmail.com>
2023-11-16 20:44:37 +0900

    feat: 마이페이지 접속시 회원과 파트너의 정보를 가져오는 기능 구현

--- replace/src/main/kotlin/com/app/replace/application/UserService.kt
+++ replace/src/main/kotlin/com/app/replace/application/UserService.kt
@@ -5,11 +5,12 @@ import com.app.replace.domain.User
 import com.app.replace.domain.UserRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
-import java.util.UUID
+import java.util.*
 
 @Service
 class UserService(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val connectionService: ConnectionService
 ) {
     fun createUser(
         email: String, password: String, nickname: String
@@ -29,6 +30,36 @@ class UserService(
             "https://replace-s3.s3.ap-northeast-2.amazonaws.com/client/profile/replace-default-profile-image.png"
         )
     }
+
+    fun loadUserInformationWithPartner(userId: Long): UserInformation {
+        val user = userRepository.findByIdOrNull(userId)
+            ?: throw IllegalArgumentException("식별자에 해당하는 유저 정보를 찾을 수 없습니다.")
+
+        val partnerId = connectionService.findPartnerIdByUserId(userId)
+
+        if (Objects.nonNull(partnerId)) {
+            val partner = userRepository.findByIdOrNull(partnerId)
+                ?: throw IllegalArgumentException("식별자에 해당하는 유저 정보를 찾을 수 없습니다.")
+            return UserInformationWithPartner(
+                SimpleUserInformation(user.nickname, "https://replace-s3.s3.ap-northeast-2.amazonaws.com/client/profile/replace-default-profile-image.png"),
+                SimpleUserInformation(partner.nickname, "https://replace-s3.s3.ap-northeast-2.amazonaws.com/client/profile/replace-default-profile-image.png")
+            )
+        }
+
+        return AloneUserInformation(SimpleUserInformation(user.nickname, "https://replace-s3.s3.ap-northeast-2.amazonaws.com/client/profile/replace-default-profile-image.png"))
+    }
 }
 
 data class SimpleUserInformation(val nickname: String, val imageUrl: String)
+
+abstract class UserInformation {
+    abstract val user: SimpleUserInformation
+}
+
+data class AloneUserInformation(override val user: SimpleUserInformation) : UserInformation()
+
+data class UserInformationWithPartner(
+    override val user: SimpleUserInformation,
+    val partner: SimpleUserInformation
+) : UserInformation()
+
