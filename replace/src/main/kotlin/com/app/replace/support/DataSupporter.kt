8fa5b862d72ce92db5fc6229e393adcc73d20a5d
백여우 <95093193+BackFoxx@users.noreply.github.com>
2023-12-12 15:13:23 +0900

    Feat/#52 db 영구보존 (#53)
    closes #52

--- replace/src/main/kotlin/com/app/replace/support/DataSupporter.kt
+++ replace/src/main/kotlin/com/app/replace/support/DataSupporter.kt
@@ -1,14 +1,13 @@
 package com.app.replace.support
 
 import com.app.replace.application.UserService
-import jakarta.annotation.PostConstruct
 import org.springframework.stereotype.Component
 
 @Component
 class DataSupporter(
     val userService: UserService
 ) {
-    @PostConstruct
+//    @PostConstruct
     fun init() {
         userService.createUser("pobi@gmail.com", "password123!", "pobi")
         userService.createUser("lisa@gmail.com", "password123!", "lisa")
